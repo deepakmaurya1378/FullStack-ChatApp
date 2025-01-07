78acfabf4de2cccfc7e0c03bcece8fa8a5acc596
Deepak Maurya <deepakmaurya1378@gmail.com>
2025-01-07 16:46:29 +0530

    theme forest

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,14 @@
- plugins: [daisyui],
+import daisyui from "daisyui"
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
   daisyui: {
     themes: [
       "light",
@@ -34,3 +44,8 @@
       "nord",
       "sunset",
     ],
+  }
+}
+
+
+      
